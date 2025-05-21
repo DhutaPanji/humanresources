73c921ef53aeccdfa1aa5bbc8c77d98f287a5b99
DhutaPanji <panzzggbanget@gmail.com>
2025-05-21 19:06:19 +0700

    memperbaiki halaman dashboard

--- .includes/sidemenu.php
+++ .includes/sidemenu.php
@@ -1,4 +1,3 @@
-<!-- Menu -->
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
   <div class="app-brand demo">
     <a href="./dashboard.php" class="app-brand-link">
@@ -10,16 +9,13 @@
   </div>
   <div class="menu-inner-shadow"></div>
   <ul class="menu-inner py-1">
-    <!-- Dashboard -->
     <li class="menu-item">
       <a href="dashboard.php" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
         <div data-i18n="Analytics">Dashboard</div>
       </a>
     </li>
-    <!-- Forms & Tables -->
     <li class="menu-header small text-uppercase"><span class="menu-header-text">MENU</span></li>
-    <!-- Forms -->
     <li class="menu-item">
       <a href="javascript:void(0);" class="menu-link menu-toggle">
         <i class="menu-icon tf-icons bx bx-detail"></i>
